9ca4401aed7d39f889145fe41602cbd19c53ce6b
Ramon <ramonlirani@hotmail.com>
2023-06-06 02:26:40 -0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-import Home from './pages/Home';
+import { Box } from '@chakra-ui/react';
+import AboutMe from './components/AboutMe/AboutMe';
+import ButtonCV from './components/ButtonCV/ButtonCV';
+import Experience from './components/Experience/Experience';
+import Contact from './components/Footer/Contact';
+import Footer from './components/Footer/Footer';
+import NameCustom from './components/NameCustom/NameCustom';
+import Navbar from './components/NavBar/NavBar';
+import Skills from './components/Skills/Skills';
 
 export function App() {
-  return <Home />;
+  return (
+    <Box
+      style={{
+        fontFamily: 'Roboto,sans-serif',
+        color: 'rgba(255, 255, 255, 0.87)',
+        backgroundColor: '#191919',
+        height: '100%',
+      }}
+    >
+      <Navbar />
+      <NameCustom />
+      <ButtonCV />
+      <AboutMe />
+      <Skills />
+      <Experience />
+      <Contact />
+      <Footer />
+    </Box>
+  );
 }
